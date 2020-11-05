92f28ff91dad21f8ba9df7d5dead2c4f6ee979c9
Helio Cunha <helioncneto@gmail.com>
2020-11-05 19:07:49 -0300

    Creating Dialog Editor

--- RPG Demo/Assets/Scripts/Dialogue/Dialogue.cs
+++ RPG Demo/Assets/Scripts/Dialogue/Dialogue.cs
@@ -7,6 +7,20 @@ namespace RPG.Dialogue
     [CreateAssetMenu(fileName = "New Dialogue", menuName = "Dialogue", order = 0)]
     public class Dialogue : ScriptableObject
     {
-        [SerializeField] DialogueNode[] dialogueNodes;
+        [SerializeField] List<DialogueNode> dialogueNodes = new List<DialogueNode>();
+
+#if UNITY_EDITOR
+        private void Awake()
+        {
+            if( dialogueNodes.Count <= 0)
+            {
+                dialogueNodes.Add(new DialogueNode());
+            }
+        }
+#endif
+        public IEnumerable<DialogueNode> GetAllNodes()
+        {
+            return dialogueNodes;
+        }
     }
 }
--- RPG Demo/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
+++ RPG Demo/Assets/Scripts/Dialogue/Editor/DialogueEditor.cs
@@ -9,14 +9,16 @@ namespace RPG.Dialogue.Editor
 {
     public class DialogueEditor : EditorWindow
     {
+        Dialogue selectedDialogue;
+
         [MenuItem("Window/Dialog Editor")]
         public static void Initialize()
         {
             GetWindow(typeof(DialogueEditor), false, "Dialogue Editor");
         }
         
-        // Cuidado ao usar OnOpenAssetAttribute, pois todo asset aberto vai rodar esse metodo, por isso devemos checar se o metodo esta sendo chamado pelo asset correto
-        [OnOpenAssetAttribute(1)]
+        // Cuidado ao usar OnOpenAsset, pois todo asset aberto vai rodar esse metodo, por isso devemos checar se o metodo esta sendo chamado pelo asset correto
+        [OnOpenAsset(1)]
         public static bool OpenDialogueEditor(int instanceID, int line)
         {
             // Retornara Null se nao for DialogEditor
@@ -27,6 +29,36 @@ namespace RPG.Dialogue.Editor
                 return true;
             }
             return false;
+        }
+
+        private void OnEnable()
+        {
+            Selection.selectionChanged += OnSelectionChanged;
+        }
+
+        private void OnSelectionChanged()
+        {
+            Dialogue dialogue = Selection.activeObject as Dialogue;
+            if(dialogue != null)
+            {
+                selectedDialogue = dialogue;
+                Repaint();
+            }
+        }
+
+        private void OnGUI()
+        {
+            if(selectedDialogue == null)
+            {
+                EditorGUILayout.LabelField("No Dialogue Selected.");
+            }
+            else
+            {
+                foreach(DialogueNode node in selectedDialogue.GetAllNodes())
+                {
+                    EditorGUILayout.LabelField(node.text);
+                }
+            }
         }
     }
 }
